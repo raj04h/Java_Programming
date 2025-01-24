0bb524a5663ad359bc5cf32abd11079c4f45b96c
raj04h <himanshuraj.hr9934@gmail.com>
2025-01-25 02:06:50 +0530

    added

--- JV_Code/student_Inheri.java
+++ JV_Code/student_Inheri.java
@@ -1,25 +1,27 @@
 //inharitence 
-public class student_Inheri { //super class
+class student{ //super class
     // if this class and its method is protected then we access it with subclass
     int roll,marks;
     String name;
+
     void inp(){ //if this method is private then it can not be asess by subclass methods
         System.out.println("Enter roll: ");
     }
 } 
-class himanshu extends student_Inheri {   // subclasses extends super classes
+class himanshu extends student{   // subclasses extends super classes
     void dis(){   //Inheritence
         roll=1;
         name="hraj";
         marks=90;
+
         System.out.println(roll+" "+name+" "+marks);
     }
+}
 
+public class student_Inheri{
     public static void main(String[] args) {
         himanshu h=new himanshu();
         h.inp();
         h.dis();
-
-
     }
 }
